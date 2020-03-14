13107f217f83964b53132025f18bab65b8cad86e
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-03-14 16:23:17 +0100

    ajout du site Lara Croft

---
+++ animation.js
@@ -0,0 +1,18 @@
+
+$(".post-wrapper").slick({
+  nextArrow: $('.next'),
+  prevArrow: $('.previous')
+});
+$(".post-wrapper02").slick({
+  nextArrow: $('.next02'),
+  prevArrow: $('.previous02')
+});
+$(".post-wrapper03").slick({
+  nextArrow: $('.next03'),
+  prevArrow: $('.previous03')
+});
+$(".post-wrapper05").slick({
+  nextArrow: $('.next05'),
+  prevArrow: $('.previous05'),
+        
+});
